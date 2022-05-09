57d21165d262e59003df998f9240388c5595f4fa
silibestref <federicosilibestre@gmail.com>
2022-05-08 22:17:30 -0300

    Se agrega validacion a formularios

--- src/App.vue
+++ src/App.vue
@@ -52,10 +52,26 @@
     <router-view></router-view>
   </div>
  
+  <footer class="bg-dark text-center text-white">
+    <div class="container p-4 pb-0">
+      <section class="mb-4">
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><font-awesome-icon icon="fa-brands fa-youtube"/></a>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><font-awesome-icon icon="circle-user"/></a>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><font-awesome-icon icon="circle-user"/></a>
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><font-awesome-icon icon="circle-user"/></a>
+      </section>
+    </div>
+    <div class="text-center p-4 div-footer">
+      © 2022 Copyright:
+      <a class="text-white" href="#">PNT2</a>
+    </div>
+  </footer>
 </div>
 </template>
 
 <style>
-/* @import "./assets/base.css"; */
+ @import "./assets/base.css";
+
+
 
 </style>
