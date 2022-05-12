99bf97f2f8b777fe16f0c91a56d8a9f94ff3a4da
silibestref <federicosilibestre@gmail.com>
2022-05-12 10:59:32 -0300

    Upgrade

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,7 @@
 
     <div class="row mt-4">
       <div class="col mb-4" v-for="pelicula of peliculas" :key="pelicula">
-        <div class="card pt-1" style="width:9rem;">
+        <div class="card" style="width:9rem;">
           <img class="img-thumbnail" style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" :src="pelicula.image">
           <div class="card-body">
             <h6 class="card-title">{{ pelicula.title }}</h6>
@@ -20,7 +20,7 @@
       </div>
     </div>  
 
-    <button @click="refrescar" type="button" class="btn btn-success">Volver</button>
+    <button v-if="visible" @click="mostrarPeliculas" type="button" class="btn btn-success">Volver</button>
 
   </div>
 </template>
@@ -34,40 +34,40 @@ export default {
   data() {
     return {
       peliculas: [],
+      listaAux: [],
       peli : {titulo:""},
+      visible: false
     };
   },
   async created() {
-    try {
-      const res = await axios.get(
-        `https://imdb-api.com/en/API/MostPopularMovies/${APIKEY}`
-      );
-      this.peliculas = res.data.items;
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  methods:{
-    buscarTitulo() {      
-      this.peliculas = this.peliculas.filter(x => x.title === this.peli.titulo)      
-    },
-    async refrescar(){
-        try {
-      const res = await axios.get(
-        `https://imdb-api.com/en/API/MostPopularMovies/${APIKEY}`
-      );
-      this.peliculas = res.data.items;
-      } catch (error) {
+      try {
+        if(this.visible){
+          this.visible = false;
+      }
+        const res = await axios.get(
+          `https://imdb-api.com/en/API/MostPopularMovies/${APIKEY}`
+        );
+        this.peliculas = res.data.items;
+        this.listaAux = [...this.peliculas];
+      } 
+      catch (error) {
         console.log(error);
       } 
+  },
+  methods:{
+    buscarTitulo() {
+      this.visible = true;
+      this.peliculas = this.peliculas.filter(x => x.title === this.peli.titulo)      
+    },
+    mostrarPeliculas(){
+      this.peliculas = [...this.listaAux];
     }
+
   }
 
 };
 </script>
 
 <style>
-
-
 
 </style>
